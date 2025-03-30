095312c9b5f0c7c1dc6a88724633cdc0c3da4240
Carlos Hernandez <carloshernandez4045@gmail.com>
2025-03-29 18:32:12 -0700

    Initial Commit

---
+++ SkySyncTests/SkySyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkySyncTests.swift
+//  SkySyncTests
+//
+//  Created by Carlos Hernandez on 3/29/25.
+//
+
+import Testing
+@testable import SkySync
+
+struct SkySyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
